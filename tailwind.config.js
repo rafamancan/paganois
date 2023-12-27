defcabcd0514f1c0a0f639c6c86ff1cac3f46252
Rafael Mançan <rafael.mancan@gmail.com>
2023-12-27 16:31:24 -0300

    feat: tailwind font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                DEFAULT: "16px",
+                sm: "24px",
+                md: "32px",
+                lg: "48px",
+                xl: "56px",
+                "2xl": "96px",
             },
         },
     },
